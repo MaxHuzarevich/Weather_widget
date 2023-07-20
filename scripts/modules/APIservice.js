e0edd49148793b1ce6dbb5a4c0dd06695f0ad0cc
MaxHuzarevich <pinsk.maks@mail.ru>
2023-07-20 15:38:46 +0300

    - add fetchWeather;

---
+++ scripts/modules/APIservice.js
@@ -0,0 +1,18 @@
+const API_URL = 'https://api.openweathermap.org/data/2.5/'
+const API_KEY = '391fdbfd3a3e77266f3d6ffc2d6c43ca';
+
+export const fetchWeather = async (city) => {
+    try {
+        const response = await fetch(`${API_URL}weather?q=${city}&appid=${API_KEY}&lang=ru`);
+        if (!response.ok) {
+            throw new Error('Ошибка запроса')
+        }
+        const data = await response.json();
+
+        return {success: true, data}
+    } catch (error) {
+        return {success: false, error}
+    }
+
+
+}
--- scripts/widget.js
+++ scripts/widget.js
@@ -1,12 +1,13 @@
 import {startWidget} from "./modules/widgetService.js";
+import {fetchWeather} from "./modules/APIservice.js";
 
 
-const initWidget = (app) => {
-
+const initWidget = async (app) => {
     const widget = startWidget();
-
     app.append(widget);
 
+    const dataWeather = await fetchWeather('Пинск');
+    console.log(dataWeather)
 }
 
 const app = document.querySelector('#app')
