ab5d94161e28964f79f6bfa54c0e770d951f5a99
MaxHuzarevich <pinsk.maks@mail.ru>
2023-07-21 15:23:50 +0300

    - render city, temp, feels_like temp

--- scripts/modules/widgetService.js
+++ scripts/modules/widgetService.js
@@ -1,12 +1,21 @@
-import {renderWidgetForecast, renderWidgetOther, renderWidgetToday} from "./render.js";
+import {renderWidgetForecast, renderWidgetOther, renderWidgetToday, showError} from "./render.js";
+import {fetchWeather} from "./APIservice.js";
 
-export const startWidget = () => {
+export const startWidget = async () => {
 
     const widget = document.createElement('div');
     widget.className = 'widget';
 
-    renderWidgetToday(widget);
-    renderWidgetOther(widget);
+    const dataWeather = await fetchWeather('Пинск');
+
+    if (dataWeather.success) {
+        renderWidgetToday(widget, dataWeather.data);
+        renderWidgetOther(widget, dataWeather.data);
+    } else {
+        showError();
+    }
+
+
     renderWidgetForecast(widget);
 
     return widget
--- scripts/widget.js
+++ scripts/widget.js
@@ -1,13 +1,9 @@
 import {startWidget} from "./modules/widgetService.js";
-import {fetchWeather} from "./modules/APIservice.js";
 
 
 const initWidget = async (app) => {
-    const widget = startWidget();
+    const widget = await startWidget();
     app.append(widget);
-
-    const dataWeather = await fetchWeather('Пинск');
-    console.log(dataWeather)
 }
 
 const app = document.querySelector('#app')
